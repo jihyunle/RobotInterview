c2f517f2b560aae20e8f7f14d042ca6473e8c3b6
jihyunle <jennifer.jlee0@gmail.com>
2019-03-27 12:44:13 -0400

    editing appeal html

--- src/main/java/edu/montgomerycollege/drdoom/Controllers/JenniferController.java
+++ src/main/java/edu/montgomerycollege/drdoom/Controllers/JenniferController.java
@@ -1,8 +1,16 @@
 package edu.montgomerycollege.drdoom.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class JenniferController
-{
+public class JenniferController {
+
+    @RequestMapping("/appeal")
+    public String appeal(@PathVariable("id") long id, Model model){
+        return "appeal";
+    }
+
 }
